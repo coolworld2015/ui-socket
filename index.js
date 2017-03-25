6bea757f4575a95e31334ce8890bf842c9325271
coolworld2015 <wintermute2015@ukr.net>
2017-03-25 20:18:13 +0200

    added WebSocket

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-var express = require("express");
+'use strict';
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
 var app = express();
- 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/chat.html');
-});
 
 app.use(express.static(__dirname + '/'));
 
-var http = require('http').createServer(app);
-var io = require('socket.io').listen(http);
+const server = express()
+	.use((req, res) => res.sendFile(INDEX) )
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-http.listen(process.env.PORT || 3000, function () {
-    console.log('Server is running on 3000');
-});
+const webSocketServer = new SocketServer({ server });
+var clients = {};
 
-io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'Welcome to the chat' });
-    socket.on('send', function (data) {
-        io.sockets.emit('message', data);
-    });
+webSocketServer.on('connection', (ws) => {
+	var id = +new Date();
+	clients[id] = ws;
+	console.log('new connection ' + id);
+	
+	var id = setInterval(function() {
+		ws.send('still alive', function() {  })
+	}, 30000)
+
+	ws.on('close', function() {
+		console.log('connection closed ' + id);
+		delete clients[id];
+	});
+
+	ws.on('message', function(message) {
+		console.log('message received ' + message);
+		for (var key in clients) {
+			clients[key].send(message);
+		}
+	});  
+  
 });
+ 
